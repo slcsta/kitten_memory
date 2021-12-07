f6d8ac3acc231d895706a4282d1ca37983288bf2
Sarah <sarahlchavez@gmail.com>
2021-12-07 11:23:26 -0800

    commented in cors and ran bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sarah = Player.create(name: "Sarah")
+elmo = Player.create(name: "Elmo")
+
+Game.create(time: "01:23", player: sarah)
+Game.create(time: "01:16", player: elmo)
